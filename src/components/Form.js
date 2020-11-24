e5bb94664ed6a1a20258e50566e54c9ecb877265
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-11-24 10:34:10 -0800

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+ 
 class Form extends React.Component {
   render() {
     return (
@@ -7,19 +7,18 @@ class Form extends React.Component {
         <form>
           <input
             type="text"
-            name="firstName"
-            onChange={event => this.props.handleChange(event)}
+            onChange={event => this.props.handleFirstNameChange(event)}
             value={this.props.formData.firstName}
           />
           <input
             type="text"
-            name="lastName"
-            onChange={event => this.props.handleChange(event)}
-            value={this.props.formData.lastName} />
+            onChange={event => this.props.handleLastNameChange(event)}
+            value={this.props.formData.lastName}
+          />
         </form>
       </div>
     )
   }
 }
-
+ 
 export default Form;
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import Form from './Form'
 import DisplayData from './DisplayData'
-
+ 
 class ParentComponent extends React.Component {
   state = {
     firstName: "",
     lastName: "",
   }
-  
-  handleChange = event => {
+ 
+  handleFirstNameChange = event => {
     this.setState({
-      [event.target.name]: event.target.value
+      firstName: event.target.value
     })
   }
-
+ 
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    })
+  }
+ 
   render() {
     return (
-      <div>
-        <Form
-          formData={this.state}
-          handleChange={this.handleChange}
-        />
-        <DisplayData formData={this.state} />
-      </div>
+        <div>
+            <Form
+                formData={this.state}
+                handleFirstNameChange={this.handleFirstNameChange}
+                handleLastNameChange={this.handleLastNameChange}
+            />
+            <DisplayData formData={this.state} />
+        </div>
     )
   }
 }
-
+ 
 export default ParentComponent;
